4fe1b57f846424c4375787d67a230dfca9c85994
aymen-nahid <85292295+aymen-nahid@users.noreply.github.com>
2021-06-12 12:33:04 +0200

    Add files via upload

---
+++ Travail2.m
@@ -0,0 +1,22 @@
+clc
+clear all
+close all
+
+%% Initialisation 
+ 
+As = -3;
+Bs = 5;
+Am = 3;
+Bm = 3;
+Gamma1 = 100;
+Gamma2 = 10;
+
+%% SISO 
+kp = 100;%100
+b0 = 1;%1
+a1 = -1;%10
+a0 = 9;%10
+bm = 3;
+am = 3;
+F = -2;
+g = 1;
